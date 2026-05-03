3cc095b3c6a96587dc4174e85ef40d1c37bb108c
Tonia Dias <tdsouzza@gmail.com>
2026-05-03 14:39:32 -0300

    chore: renomeia testes de e2e para api e atualiza configuração do Playwright

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './api',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
