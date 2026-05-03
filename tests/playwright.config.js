26e8ca4bd2c798c4121e286eeab57a0b8d6e4c08
Tonia Dias <tdsouzza@gmail.com>
2026-05-03 12:20:26 -0300

    fix: adiciona BASE_URL ao workflow para corrigir erro de Invalid URL

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:3001',
     extraHTTPHeaders: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
